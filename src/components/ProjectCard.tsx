574015ccaeac4fd5cb25595b329f36a2c8224802
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2025-03-30 00:02:35 +0100

    Added transition feature on modal expand view

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -91,6 +91,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut"}}
             onClick={toggleExpand} // Close modal when clicking the backdrop
           >
             <motion.div
