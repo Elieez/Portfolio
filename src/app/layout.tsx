42c51ae17d8e57946d4b92dedb4e164f72e21782
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2025-03-31 16:05:55 +0200

    Added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,22 @@ import Navbar from "@/components/Navbar";
 
 export const metadata = {
   title: "Elie Bou Absi - System Developer",
-  description: "Portfolio of Elie Bou Absi, a system developer focused on building scalable digital solutions.",
+  description:
+    "Portfolio of Elie Bou Absi, a system developer focused on building scalable digital solutions.",
+  icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+      { url: '/my-favicon/web-app-manifest-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/my-favicon/web-app-manifest-512x512.png', sizes: '512x512', type: 'image/png' },
+      { url: '/my-favicon/icon0.svg', type: 'image/svg+xml' },
+      { url: '/my-favicon/icon1.png', sizes: '96x96', type: 'image/png' },
+    ],
+    apple: '/my-favicon/apple-icon.png',
+  },
+  manifest: '/my-favicon/manifest.json',
+  appleWebApp: {
+    title: "Elie's Portfolio",
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
