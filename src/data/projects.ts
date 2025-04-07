2fbc9d5263f11e86de76df8e4baa625955bfdb0d
Elie Bou Absi <135924664+Elieez@users.noreply.github.com>
2025-04-07 14:46:07 +0200

    Project desc

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,17 +13,16 @@ export interface Project {
   export const projects: Project[] = [
     {
       id: "1",
-      title: "E-commerce Platform",
-      description: "A full-stack e-commerce solution with React and Node.js.",
-      image: "/images/AZDOI.PNG",
-      github: "https://github.com/yourusername/ecommerce-platform",
-      website: "https://yourwebsite.com/ecommerce-platform",
-    },
-    {
-      id: "2",
       title: "AZDOI",
-      description:"AZDOI is a NuGet package developed during my internship at WCOM. It streamlines the process of generating markdown documentation and visual representations for Azure DevOps organization, making it easier to integrate and report on DevOps data.",
-      longDescription: "This is a sample long description for the project.",
+      description:`AZDOI is a .NET tool that automates documentation for Azure DevOps organizations 
+      by inventorying projects and repositories via the Azure DevOps REST API, then generating structured Markdown files for static site creation.`,
+
+      longDescription: `AZDOI is a .NET tool developed at WCOM that streamlines the documentation of Azure DevOps organizations. 
+      It connects to the Azure DevOps REST API using a personal access token or Azure Entra ID, 
+      inventories projects and repositories—including branches, tags, and README content—and organizes the data into Markdown files. 
+      These files serve as a comprehensive source for building a static website.
+      Future updates may expand documentation to cover work items, pipelines, and release definitions.`,
+      
       image: "/images/AZDOI.PNG",
       github: "https://github.com/WCOMAB/AZDOI",
       nuget: "https://www.nuget.org/packages/AZDOI",
